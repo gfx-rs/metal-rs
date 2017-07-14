f8a23acbef270b1d5a75ec8e070df7fc6b0a229e
Dzmitry Malyshau <kvarkus@gmail.com>
2017-07-14 15:52:54 -0400

    Licence headers updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
-// Copyright 2016 metal-rs developers
+// Copyright 2017 GFX developers
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
-
-#![crate_name = "metal_rs"]
-#![crate_type = "rlib"]
 
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
