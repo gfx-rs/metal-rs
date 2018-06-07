e23de86ea97a9520e3ed1e190c048f05681e4445
Shawn Tuh <thomspoon@Shawns-MacBook-Pro.local>
2018-06-06 19:01:58 -0600

    Adding missing MTLFeatureSets

--- src/device.rs
+++ src/device.rs
@@ -37,13 +37,17 @@ pub enum MTLFeatureSet {
     iOS_GPUFamily1_v4 = 8,
     iOS_GPUFamily2_v4 = 9,
     iOS_GPUFamily3_v3 = 10,
+    iOS_GPUFamily4_v1 = 11,
     tvOS_GPUFamily1_v1 = 30000,
     tvOS_GPUFamily1_v2 = 30001,
     tvOS_GPUFamily1_v3 = 30002,
+    tvOS_GPUFamily2_v1 = 30003,
     macOS_GPUFamily1_v1 = 10000,
     macOS_GPUFamily1_v2 = 10001,
     macOS_ReadWriteTextureTier2 = 10002,
     macOS_GPUFamily1_v3 = 10003,
+    macOS_GPUFamily1_v4 = 10004,
+    macOS_GPUFamily2_v1 = 10005,
 }
 
 #[allow(non_camel_case_types)]
