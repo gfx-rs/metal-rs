a99e0bd62c2caff7a4f16a89280e8ce874cb0d9d
Joshua Groves <josh@joshgroves.com>
2018-08-03 22:54:45 -0600

    Deprecate metal-rs

--- examples/caps/main.rs
+++ examples/caps/main.rs
@@ -5,7 +5,7 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-extern crate metal;
+extern crate metal_rs as metal;
 
 use metal::*;
 
