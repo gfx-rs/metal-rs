91b0339eccbea678f7fbdbce148b5049e26655f5
Adam Nemecek <adamnemecek@gmail.com>
2020-03-05 22:10:49 -0800

    fixed the type of commandbufferhandler

--- src/commandbuffer.rs
+++ src/commandbuffer.rs
@@ -11,6 +11,7 @@ use block::Block;
 
 #[repr(u32)]
 #[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum MTLCommandBufferStatus {
     NotEnqueued = 0,
     Enqueued = 1,
@@ -22,6 +23,7 @@ pub enum MTLCommandBufferStatus {
 
 #[repr(u32)]
 #[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum MTLCommandBufferError {
     None = 0,
     Internal = 1,
@@ -37,12 +39,13 @@ pub enum MTLCommandBufferError {
 
 #[repr(u32)]
 #[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum MTLDispatchType {
     Serial = 0,
     Concurrent = 1,
 }
 
-type CommandBufferHandler = Block<(CommandBufferRef,), ()>;
+type CommandBufferHandler<'a> = Block<(&'a CommandBufferRef,), ()>;
 
 pub enum MTLCommandBuffer {}
 
