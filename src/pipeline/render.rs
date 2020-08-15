c3e29735bf843449ce9585e0911b09e510480e95
adamnemecek <adamnemecek@gmail.com>
2020-08-15 14:10:04 -0700

    renderpipelinestate only has a label getter, you set the label on the descriptor (#175)

--- src/pipeline/render.rs
+++ src/pipeline/render.rs
@@ -398,13 +398,6 @@ impl RenderPipelineStateRef {
             crate::nsstring_as_str(label)
         }
     }
-
-    pub fn set_label(&self, label: &str) {
-        unsafe {
-            let nslabel = crate::nsstring_from_str(label);
-            let () = msg_send![self, setLabel: nslabel];
-        }
-    }
 }
 
 pub enum MTLRenderPipelineColorAttachmentDescriptorArray {}
