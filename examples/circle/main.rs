0f4954a7d2ce3c1d62e547010cf108c4e562d381
Jinlei Li <jinleili0@outlook.com>
2022-09-05 15:36:20 +0800

    Set color space on circle example

--- examples/circle/main.rs
+++ examples/circle/main.rs
@@ -86,7 +86,10 @@ fn main() {
     layer.set_device(&device);
     layer.set_pixel_format(MTLPixelFormat::BGRA8Unorm);
     layer.set_presents_with_transaction(false);
-
+    // Set color space
+    if let Some(cs) = ColorSpace::create_with_name(ColorSpaceName::DisplayP3) {
+        layer.set_color_space(cs);
+    }
     unsafe {
         let view = window.ns_view() as cocoa_id;
         view.setWantsLayer(YES);
